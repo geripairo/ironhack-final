bb8fd71f4e1c45651656d25147dc74952ca1f0ef
Gerard Pairo <gerardpairo@hotmail.com>
2022-10-22 17:50:30 +0200

    adding some icons

--- ironhack-final/src/views/Dashboard.vue
+++ ironhack-final/src/views/Dashboard.vue
@@ -1,30 +1,45 @@
 <template>
-    <div class="container mt-10 px-4">
-  
-      <div v-if="data.length === 0" class="w-full flex flex-col items-center">
-        <h1 class="text-2xl text-black">This is empty...</h1>
-        <router-link
-        class="mt-6 py-2 px-6 rounded-md text-sm text-white bg-black" 
-        :to="{name: 'Create'}"> Create new Workout</router-link>
-      </div>
-  
-      <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        <router-link 
-        class="flex-flex-col items-center bg-white p-8 shadow-md cursor-pointer"
-        :to="{name: ''}"
-        v-for="(workout, index) in data"
-        :key="index"
-        >
-        <p class="mt-6 py-1 px-3 text-xs text-black bg-white shadow-md rounded-md">
-          {{workout.workoutType}}
-        </p>
-  
-        <h1 class="mt-8 mb-2 text-center text-xl text-black">
-          {{workout.workoutName}}
-        </h1>
-        </router-link>
-      </div>
-    </div>
+    
+        <div class="container mt-10 px-4">
+    
+        <div v-if="data.length === 0" class="w-full flex flex-col items-center">
+            <h1 class="text-2xl text-black">This is empty...</h1>
+            <router-link
+            class="mt-6 py-2 px-6 rounded-md text-sm text-white bg-black" 
+            :to="{name: 'Create'}"> Create new Workout</router-link>
+        </div>
+    
+        <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <router-link 
+            class="flex-flex-col items-center bg-white p-8 shadow-md cursor-pointer"
+            :to="{name: ''}"
+            v-for="(workout, index) in data"
+            :key="index"
+            >
+              <img 
+                v-if="workout.workoutType === 'cardio'" 
+                src="../assets/images/logo-img.png" 
+                alt="test-img1"
+                class="h-24 w-auto"
+              />
+
+              <img 
+                v-else 
+                src="../assets/images/logo-full.png"
+                alt="test-img2"
+                class="h-24 w-auto"
+              />
+
+              <p class="mt-6 py-1 px-3 text-xs text-black bg-white shadow-md rounded-md">
+              {{workout.workoutType}}
+              </p>
+      
+              <h1 class="mt-8 mb-2 text-center text-xl text-black">
+              {{workout.workoutName}}
+              </h1>
+            </router-link>
+        </div>
+        </div>
   </template>
   
   <script setup>
@@ -47,7 +62,6 @@
   const userStore = useUserStore();
   const taskStore = useTaskStore();
   const data =  computed(() => taskStore.data);
-  const dataLoaded = taskStore.dataLoaded;
   const user = userStore.user;
   
   onMounted(function() {
